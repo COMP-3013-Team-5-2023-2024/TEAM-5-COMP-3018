7e4db5d4c3b7c1957f7852c2220cf00ecf928161
psymf6 <psymf6@nottingham.ac.uk>
2023-11-02 15:43:42 +0000

    made a start to the pin page

---
+++ app/src/main/java/com/example/group_5_project/PinPage.java
@@ -0,0 +1,14 @@
+package com.example.group_5_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PinPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pin_page);
+    }
+}
