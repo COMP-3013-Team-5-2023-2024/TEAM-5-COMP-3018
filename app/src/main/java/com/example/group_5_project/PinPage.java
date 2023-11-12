e537c71663c37af3185d7d10945597e579bc4ccb
psymf6 <psymf6@nottingham.ac.uk>
2023-11-12 16:38:26 +0000

    altered pin page to set a pin

--- app/src/main/java/com/example/group_5_project/PinPage.java
+++ app/src/main/java/com/example/group_5_project/PinPage.java
@@ -4,16 +4,17 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 
 public class PinPage extends AppCompatActivity implements View.OnClickListener {
+
     public static final int PIN_LENGTH = 4;
     View[] dots = new View[4];
-    private String PinToTest = "";
-
-    // could add fail limit
+    private String pinToTest = "";
+    private PinPageState saveCheck;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,7 +27,20 @@ public class PinPage extends AppCompatActivity implements View.OnClickListener {
         dots[2] = findViewById(R.id.digit_display_03);
         dots[3] = findViewById(R.id.digit_display_04);
 
+        // need to check to see what type of application they want
+        saveCheck = PinPageState.SAVE;
     }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        // TODO check to see if this happens when final is called
+        if (this.saveCheck == PinPageState.SAVE) {
+            setStoredPin("");
+        }
+    }
+
+    // could add fail limit
 
     @Override
     public void onClick(View view) {
@@ -73,9 +87,9 @@ public class PinPage extends AppCompatActivity implements View.OnClickListener {
 
     }
 
-    public void updateDisplay() {
+    private void updateDisplay() {
         for (int i = 0; i < PIN_LENGTH; i++) {
-            if (PinToTest.length() <= i) {
+            if (pinToTest.length() <= i) {
                 dots[i].setBackgroundResource(R.drawable.pin_page_no_digit);
             } else {
                 dots[i].setBackgroundResource(R.drawable.pin_page_digit);
@@ -84,11 +98,13 @@ public class PinPage extends AppCompatActivity implements View.OnClickListener {
     }
 
     private void addToPin(char num) {
-        if (PinToTest.length() < PIN_LENGTH) {
-            PinToTest += num;
+        if (pinToTest.length() < PIN_LENGTH) {
+            pinToTest += num;
         }
     }
 
+    
+    // TODO should be part of another class
     private String getStoredPin() {
         // need to actually implement this thing
         String pin = "2345";
@@ -98,22 +114,69 @@ public class PinPage extends AppCompatActivity implements View.OnClickListener {
         return pin;
     }
 
+
+    // TODO should be part of another class
+    private Void setStoredPin(String pin) {
+        // PinToTest
+        return null;
+    }
+
     private void removeFromPin() {
-        if (PinToTest.length() >= 1) {
-            PinToTest = PinToTest.substring(0, PinToTest.length() - 1);
+        if (pinToTest.length() >= 1) {
+            pinToTest = pinToTest.substring(0, pinToTest.length() - 1);
         }
     }
 
     private void end() {
-
-        // could make an animation play on failure
-        if (PinToTest.equals(getStoredPin())) {
-            Intent intent = new Intent();
-            setResult(Activity.RESULT_OK, intent);
-            finish();
+        if (saveCheck.equals(PinPageState.SAVE)) {
+            checkEnd();
         } else {
-            PinToTest = "";
-            updateDisplay();
+            saveEnd();
         }
+    }
+
+    private void saveEnd() {
+        if (getStoredPin().equals("")) {
+            setStoredPin(pinToTest);
+            Toast.makeText(this, "please enter the pin again", Toast.LENGTH_SHORT).show();
+            resetPinPage();
+            return;
+        } else if (getStoredPin().equals(pinToTest)) {
+            finishPinPage();
+            return;
+        }
+        setStoredPin("");
+        badPinAnimation();
+        resetPinPage();
+    }
+
+    private void badPinAnimation() {
+        Toast.makeText(this, "not the same pin", Toast.LENGTH_SHORT).show();
+        // TODO add a failure thing here if they get it wrong
+    }
+
+    private void resetPinPage() {
+        pinToTest = "";
+        updateDisplay();
+    }
+
+    private void finishPinPage() {
+        Intent intent = new Intent();
+        setResult(Activity.RESULT_OK, intent);
+        finish();
+    }
+
+    private void checkEnd() {
+        if (pinToTest.equals(getStoredPin())) {
+            finishPinPage();
+        } else {
+            badPinAnimation();
+            resetPinPage();
+        }
+    }
+
+    public enum PinPageState {
+        SAVE,
+        CHECK
     }
 }
