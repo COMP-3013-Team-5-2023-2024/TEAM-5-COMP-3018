53ce9c97b48f019974d0fbb4f6084b97822e63b8
Harry Ramsey <harryramseybusiness@gmail.com>
2023-11-27 16:11:40 +0000

    Allow user to input a date to recieve period estimates
    This commit adds a material design date picker to allow the user to
    input their dates to recieve a prediction about their period.

--- app/src/main/java/com/example/periodtracker/MainActivity.java
+++ app/src/main/java/com/example/periodtracker/MainActivity.java
@@ -5,7 +5,6 @@ import android.Manifest;
 import android.os.Bundle;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
-
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.view.View;
@@ -14,6 +13,13 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.datepicker.MaterialDatePicker;
+import com.google.android.material.datepicker.MaterialPickerOnPositiveButtonClickListener;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 import java.util.Calendar;
 import java.util.Locale;
 
@@ -32,10 +38,16 @@ public class MainActivity extends AppCompatActivity {
             Manifest.permission.POST_NOTIFICATIONS,
     };
 
+    private TextView minPrediction;
+    private TextView maxPrediction;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        minPrediction = findViewById(R.id.minPredictionText);
+        maxPrediction = findViewById(R.id.maxPredictionText);
 
         buttonClickAnimation = new AlphaAnimation(1,0);
         buttonClickAnimation.setDuration(300);
@@ -97,6 +109,26 @@ public class MainActivity extends AppCompatActivity {
                 });
         multiplePermLauncher.launch(REQUIRED_PERMISSIONS);
 
+    }
+
+    public void onSelectDateClick(View view) {
+        MaterialDatePicker<Long> materialDatePicker = MaterialDatePicker.Builder.datePicker()
+                .setTitleText(getString(R.string.select_date))
+                .setSelection(MaterialDatePicker.todayInUtcMilliseconds())
+                .build();
+        materialDatePicker.addOnPositiveButtonClickListener(new MaterialPickerOnPositiveButtonClickListener<Long>() {
+            @Override
+            public void onPositiveButtonClick(Long selection) {
+                LocalDateTime dateTime = LocalDateTime.ofInstant(Instant.ofEpochMilli(selection), ZoneId.systemDefault());
+                LocalDate selectedDate = dateTime.toLocalDate();
+                MenstrualCycleTracker.PeriodPrediction prediction = MenstrualCycleTracker.trackMenstrualCycle(selectedDate);
+                DateTimeFormatter format = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+                minPrediction.setText(prediction.minEndDate.format(format));
+                maxPrediction.setText(prediction.maxEndDate.format(format));
+            }
+        });
+
+        materialDatePicker.show(getSupportFragmentManager(), "tag");
     }
 
 }
